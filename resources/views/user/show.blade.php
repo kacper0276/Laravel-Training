6d31cd1c4a58a928594ef99b3e06f7c336dba39a
kacper0276 <kacper0276@op.pl>
2024-11-20 21:29:37 +0100

    If, ElseIf, Else - Blade

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -14,6 +14,17 @@
         <li>Imię: {{ $user['firstName'] }}</li>
         <li>Nazwisko: {{ $user['lastName'] }}</li>
         <li>Miasto: {{ $user['city'] }}</li>
+
+        <li>
+            Wiek: {{ $user['age'] }}
+            @if ($user['age'] >= 18)
+                <div>Osoba dorosła</div>
+            @elseif ($user['age'] >= 16)
+                <div>Prawie dorosła</div>
+            @else
+                <div>Nastolatek</div>
+            @endif
+        </li>
     </ul>
 
     <div>
