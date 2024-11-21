a3ec70330cfc29f58ee9da35430d799ae87fd5d6
kacper0276 <kacper0276@op.pl>
2024-11-21 11:12:38 +0100

    @For and @break and @continue

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -13,8 +13,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td colspan="3">FOREACH</td>
+                    <tr><td colspan="3">FOREACH</td></tr>
                     @foreach ($users as $user)
                         <tr>
                             <td>{{ $user['id'] }}</td>
@@ -30,7 +29,35 @@
                             </td>
                         </tr>
                     @endforeach
-                </tr>
+
+                    <tr><td colspan="3">FOR</td></tr>
+                    @for ($i = 0; $i < count($users); $i++)
+                        @if ($i == 2)
+                            @continue
+                        @endif
+                        {{-- LUB --}}
+                        {{-- @continue($i == 2) równoważny zapis--}}
+                        <tr>
+                            <td>{{ $users[$i]['id'] }}</td>
+                            <td>{{ $users[$i]['name'] }}</td>
+                            <td>
+                                <a href="{{
+                                    route('blade.user.details', [
+                                    'userId' => $users[$i]['id']
+                                    ])
+                                }}">
+                                    Szczegóły
+                                </a>
+                            </td>
+                        </tr>
+                        @if ($i == 1)
+                            @break
+                        @endif
+                        {{-- LUB --}}
+                        {{-- @break($i == 1) - równoważny zapis --}}
+                    @endfor
+
+
             </tbody>
         </taable>
     </html>
