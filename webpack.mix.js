8c16488d524f01cd026c3901a074af2a5c02711c
kacper0276 <kacper0276@op.pl>
2024-11-25 19:38:48 +0100

    Merge files created in public (not resources)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
+    .scripts(["public/js/admin.js", "public/js/admin2.js"], "public/js/all.js")
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/admin.scss", "public/css/admin");
