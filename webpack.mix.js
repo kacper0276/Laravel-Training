e54c9cb87c3ceecfda46138c309789cb114930a4
kacper0276 <kacper0276@op.pl>
2024-11-25 19:46:50 +0100

    Add file version in mix while is production

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,7 @@ mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/admin.scss", "public/css/admin")
     .version();
+
+if (mix.inProduction()) {
+    mix.version();
+}
