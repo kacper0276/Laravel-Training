2264d6d7bf2a6802b98aca52449447b94288e0d4
kacper0276 <kacper0276@op.pl>
2024-11-25 19:34:58 +0100

    Add new scss file

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,4 @@ let mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
-    .styles(
-        ["resources/sass/styles1.css", "resources/sass/styles2.css"],
-        "public/css/plaing.css"
-    );
+    .sass("resources/sass/admin.scss", "public/css/admin");
