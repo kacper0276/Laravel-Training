0ee0d09cdbf8fd8a34b6d04caada39ab1a4d4a00
kacper0276 <kacper0276@op.pl>
2024-11-25 19:31:20 +0100

    Merge css files into one css file

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@ let mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
-    .less("resources/sass/app.less");
+    .styles(
+        ["resources/sass/styles1.css", "resources/sass/styles2.css"],
+        "public/css/plaing.css"
+    );
